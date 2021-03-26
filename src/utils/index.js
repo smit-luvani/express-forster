92a784ed20d127ca829465c60bef6ff8625fe086
Smit Luvani <svluvani@gmail.com>
2021-03-26 22:41:40 +0100

    fix: index file path

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,7 +4,7 @@
  */
 
 module.exports.arrayItemInArrayUtil = require('./array-item-in-array')
-module.exports.randomGenerateUtil = require('./array-item-in-array')
-module.exports.regexValidateUtil = require('./array-item-in-array')
-module.exports.timeValidationUtil = require('./array-item-in-array')
-module.exports.responseUtil = require('./array-item-in-array')
+module.exports.randomGenerateUtil = require('./random')
+module.exports.regexValidateUtil = require('./regex')
+module.exports.timeValidationUtil = require('./time-validation')
+module.exports.responseUtil = require('./response')
