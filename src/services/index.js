b2cae616119643c1c992b8cc7371db2e004d8ed7
Smit Luvani <svluvani@gmail.com>
2022-11-30 04:55:19 +0100

    update: comment out services export
    - Developer has to remove comment as per necessity of project

--- src/services/index.js
+++ src/services/index.js
@@ -1,23 +1,17 @@
 /**
  * @author Smit Luvani
  * @description Distribute All Service to Application
- * - [RECOMMEND] Use Particular Service instead of all service or remove unwanted service
+ * - Remove comment from below line to use service
+ * - Enable/Disable Service wise Logging in config/default.js file
  */
 
-module.exports = {
-    winston: require('./winston'),
-    mongoose: require('./mongoose'),
-    firebaseAdmin: require('./firebase-admin'),
-    jwt: require('./jwt'),
-    bcryptjs: require('./bcryptjs'),
-    razorpay: require('./razorpay'),
-    aws: require('./aws-sdk'),
-    multerS3: require('./multer-s3'),
-    multer: require('./multer'),
-    nodemailer: require('./nodemailer'),
-};
-
-// To Disable, Use Single Line Comment
-
-// Service Log
-// Enable/Disable Service Logging in config/default.js file
+module.exports.winston = require('./winston')
+// module.exports.mongoose = require('./mongoose')
+// module.exports.firebaseAdmin = require('./firebase-admin')
+// module.exports.jwt = require('./jwt')
+// module.exports.bcryptjs = require('./bcryptjs')
+// module.exports.razorpay = require('./razorpay')
+// module.exports.aws = require('./aws-sdk')
+// module.exports.multerS3 = require('./multer-s3')
+// module.exports.multer = require('./multer')
+// module.exports.nodemailer = require('./nodemailer')
