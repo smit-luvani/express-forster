669e8399b4bf79b097b312f9c127cdb2a1400bb8
Smit Luvani <svluvani@gmail.com>
2022-12-29 18:12:01 +0100

    fix(regex): email

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -6,11 +6,11 @@
 const { winston: logger } = require('../services'),
     unirest = require('unirest')
 
-module.exports.email = async(value, checkDisposable) => {
+module.exports.email = async (value, checkDisposable) => {
     let regex = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
     if (value != "") {
-        if (!value.match(regex) || value.match(regex) != value) {
+        if (!value.match(regex) || value.match(regex)[0] != value) {
             logger.error('Incorrect Email Value. Not match with regex');
             return false;
         }
