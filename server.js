54993ea1a2ba73d444308679653ead5dfe46078e
Smit Luvani <svluvani@gmail.com>
2023-02-22 13:20:32 +0100

    update: used http package to start server

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ require('dotenv').config({ path: `./.env.${process.env.NODE_ENV || 'development'
 if (!process.env.NODE_ENV || !process.env.PORT) throw new Error('NODE_ENV or PORT not found in .env file. Please configure .env file. See README.md')
 
 const logger = require('./src/services/winston');
+const packageJson = require('./package.json');
+const expressApp = require('./app');
+const http = require('http');
 
-// App entry point
-require('./app').listen(process.env.PORT).addListener('listening', () => {
-    console.clear() // Comment this for Continuos logging
-    logger.info(`[Server.js]: \x1b[32m\x1b[1m PORT: ${process.env.PORT} \x1b[0m || \x1b[32m\x1b[1m NODE_ENV: ${process.env.NODE_ENV || '\x1b[31m\x1b[1m NODE_ENV NOT FOUND'} \x1b[0m`)
-})
+console.clear() // Comment this for Continuos logging
+
+// HTTP Server
+const server = http.createServer(expressApp);
+server.addListener('listening', () => {
+    logger.info(`[Server.js]: \x1b[96m\x1b[1m PROJECT: ${packageJson.name} \x1b[0m | \x1b[32m\x1b[1m PORT: ${process.env.PORT} \x1b[0m | \x1b[32m\x1b[1m NODE_ENV: ${process.env.NODE_ENV || '\x1b[31m\x1b[1m NODE_ENV NOT FOUND'} \x1b[0m`)
+});
+
+server.listen(process.env.PORT); // Start Server on PORT
